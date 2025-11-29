2d0c4be9061499c4187c15178c7f3a767159ff4a
RICARDO ORTELLADO <sansumghros@gmail.com>
2025-11-29 22:29:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7905412770-89bf5",
+  "appId": "1:1088552341362:web:ab8ca6f9994189f19fd7eb",
+  "apiKey": "AIzaSyDZUYQ9ErB6x34xl9LXgR6uU-HMbRQyT2A",
+  "authDomain": "studio-7905412770-89bf5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1088552341362"
+};
